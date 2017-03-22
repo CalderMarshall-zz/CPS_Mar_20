3c99254aa8c19fce87915bbb6a59fc682ac26520
CalderMarshall <cmarshall72@gmail.com>
2017-03-21 22:12:11 -0400

    attempted resize program

--- resize.js
+++ resize.js
@@ -1,28 +1,38 @@
 var fs = require('fs');
 var gm = require('gm');
+var request = require('request');
 
 // sets up read lines
 var rl = readline.createInterface({
      input: process.stdin,
      output: process.stdout,
 });
+var image = "https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png";
 
+request(image, function (error, response, html){
+     console.log('error: ' error);
+     gm(image)
+          .resize(240,240)
+          .write()
+})
 //asks for url
-     rl.question("image url:", function (url){
-          // asks file to save to
-     gm(url)
-          rl.question("Width and Height: ", function (width, height) {
-               .resize(width, height)
-               // fetches data from page
-               request.get(url,function(err,resp,pagecontent){
-                    // writes data to save to file
-                         fs.writeFile(page, pagecontent, function(err){
-                         if (err) {
-                              console.log(err.message);
-                              return;
-                         }
-                         console.log("Saved to file: " + page);
-                    });
-          });
-     });
-});
+//           var options = "https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png"
+//           request (options, function(err, response, html) {
+//                console.log('error:', error);
+//           .resize(width, height)
+//                // writes data to sav
+//                     .write(url), function(err){
+//                     if (err) {
+//                          console.log(err.message);
+//                          return;
+//                     }
+//                     console.log("Saved to file: " + page);
+//
+//                     gm(url)
+//           });
+//           // asks file to save to
+//
+//
+//
+//
+// });
